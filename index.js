fd826fed6f3c875052a0cff309850167da92ecbc
AngelikaWidz <angelikawidz@yahoo.com>
2024-04-25 17:05:27 +0100

    js created variables which select random number 2 - 12 and add together

---
+++ index.js
@@ -0,0 +1,8 @@
+// 2 variables - firstCard + secondCard with values to random number between 2 - 11
+
+let firstCard = Math.floor(Math.random()*12)+2;
+let secondCard = Math.floor(Math.random()*12)+2;
+let total = firstCard + secondCard;
+
+console.log(total);
+// variable, sum and set to sum of 2 cards
