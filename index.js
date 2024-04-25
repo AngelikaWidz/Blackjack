8889f3c32ddfbeac7be3654159fd472b32904195
AngelikaWidz <angelikawidz@yahoo.com>
2024-04-25 17:51:44 +0100

    added if else statement

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // 2 variables - firstCard + secondCard with values to random number between 2 - 11
 
-let firstCard = Math.floor(Math.random()*12)+2;
-let secondCard = Math.floor(Math.random()*12)+2;
+let firstCard = Math.floor(Math.random() * 12) + 2;
+let secondCard = Math.floor(Math.random() * 12) + 2;
 let total = firstCard + secondCard;
 
-console.log(total);
+//  
 // variable, sum and set to sum of 2 cards
+
+if (total < 21) {
+console.log(total + "congrats");
+} else if (total > 21) {
+console.log(total + "bad")
+} ;
