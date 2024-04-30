fd82b02db89470291f537d22dd2d7eb33b75dbcf
AngelikaWidz <angelikawidz@yahoo.com>
2024-04-30 15:12:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 
 let firstCard = Math.floor(Math.random() * 12) + 2
 let secondCard = Math.floor(Math.random() * 12) + 2
-
 // variable, sum and set to sum of 2 cards
 let total = firstCard + secondCard;
+// if the user has Blackjack - used for cashing out
+let hasBlackjack = false
+let isAlive = true
 
 // if .. else statement which looks at the total, and then decides which codeblock to run depending on the sum of 2 cards compared to 21
 // using ternary operator
+
 total < 21?
     console.log(total + " congrats, draw a new card?")
 : total === 21?
-    console.log(total + " Winner!")
-: (console.log(total + " Bust"));
+    console.log(total + " Winner!", hasBlackjack = true) 
+: console.log(total + " Bust" ), isAlive = false
