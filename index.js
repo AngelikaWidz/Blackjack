11f6c5b425555bf5934f42a7c903c8ed1e31a698
AngelikaWidz <angelikawidz@yahoo.com>
2024-04-30 12:40:23 +0100

    added condition for === 21

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // 2 variables - firstCard + secondCard with values to random number between 2 - 11
 
-let firstCard = Math.floor(Math.random() * 12) + 2;
-let secondCard = Math.floor(Math.random() * 12) + 2;
+let firstCard = Math.floor(Math.random() * 12) + 2
+let secondCard = Math.floor(Math.random() * 12) + 2
+
+// variable, sum and set to sum of 2 cards
 let total = firstCard + secondCard;
 
-//  
-// variable, sum and set to sum of 2 cards
-
+// if .. else statement which looks at the total, and then decides which codeblock to run depending on the sum of 2 cards compared to 21
 if (total < 21) {
-console.log(total + "congrats");
+    console.log(total + " congrats, draw a new card?")
 } else if (total > 21) {
-console.log(total + "bad")
-} ;
+    console.log(total + " Bust!")
+} else if (total === 21) {
+    console.log(total + " Winner!")
+}
